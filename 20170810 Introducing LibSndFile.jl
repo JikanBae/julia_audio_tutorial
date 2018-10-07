3e97ec437363f71119715d316d2af3c0e44c18d0
MARUI Atsushi <marui@tunguska.local>
2018-10-07 15:17:59 +0900

    1.0に対応
    * linspace --> range
    * FFTW、LibSndFileのusing
    * PyPlot --> Plots

--- 20170810 Introducing LibSndFile.jl
+++ 20170810 Introducing LibSndFile.jl
@@ -1,9 +1,10 @@
 
-## Pkg.add("LibSndFile")   # 初回のみ
-
-using LibSndFile       # 音ファイルの読み書きに使う
+using FileIO: load, save
+import LibSndFile      # 音ファイルの読み書きに使う
 using SampledSignals   # オーディオサンプルの定義
-using PyPlot           # グラフ描画に使う
+using FFTW             # FFTのため
+using Plots            # グラフ描画に使う
+pyplot();
 
 x = load("recorder.flac")
 
@@ -13,32 +14,19 @@ x.data
 
 x[1:44100, :]
 
-x1 = x.data[:, 1];   # チャンネル1のみ抜き出し
+x1 = x.data[1:round(Int, 1.8*fs), 1];   # チャンネル1のみ抜き出し
 t = (0 : length(x1)-1) / x.samplerate;
-plot(t, x1);
-ylim(-1, +1);
-grid();
-title("Music Excerpt");
-xlabel("Time (s)");
-ylabel("Amplitude");
-#savefig("output.png")
+plot(t, x1, ylim=(-1, +1), title="Music Excerpt", xlabel="Time (s)", ylabel="Amplitude", legend=false)
 
 X1 = abs.(fft(x1));
-X2 = X1[1 : Int(length(X1)/2) + 1];
+X2 = X1[1 : round(Int, length(X1)/2 + 1)];
 X2[2:end-1] = X2[2:end-1] * 2.0;
-f = linspace(0, x.samplerate/2, length(X2));
-plot(f, X2);
-grid();
-title("Amplitude Spectrum");
-xlabel("Frequency (Hz)");
-ylabel("Amplitude");
-#savefig("output2.png");
+f = range(0, stop=x.samplerate/2, length=length(X2));
+plot(f, X2,
+    title="Amplitude Spectrum", xlabel="Frequency (Hz)", ylabel="Amplitude", legend=false)
 
-semilogx(f, 10*log10.(X2.^2));
-grid();
-title("Power Spectrum");
-xlabel("Frequency (Hz)");
-ylabel("Power (dB)");
+plot(f, 20*log10.(X2 / maximum(X2)),
+    title="Power Spectrum", xlabel="Frequency (Hz)", ylabel="Power (dB)", legend=false)
 
 y = load("recorder.flac");
 fs = y.samplerate;
@@ -52,18 +40,16 @@ Y3 = Y2[1 : Int(length(Y2)/2) + 1];
 Y3[2:end-1] = Y3[2:end-1] * 2.0;
 Y4 = 10*log10.(Y3);
 
-f = linspace(0, y.samplerate/2, length(Y3));
-plot(f, Y4-maximum(Y4));
-xlim(0, 15000);
-ylim(-80, +3);
-grid();
-title("Power Spectrum");
-xlabel("Frequency (Hz)");
-ylabel("Power (dB)");
-#savefig("output3.png");
+f = range(0, stop=y.samplerate/2, length=length(Y3));
+plot(f, Y4 .- maximum(Y4),
+    xlim=(0, 15000), ylim=(-80, +3),
+    title="Power Spectrum", xlabel="Frequency (Hz)", ylabel="Power (dB)",
+    #xtick=([31.5, 63, 125, 250, 500, 1000, 2000, 4000, 8000, 16000],
+    #    ["31.5", "63", "125", "250", "500", "1k", "2k", "4k", "8k", "16k"]),
+    legend=false)
 
 # writing sound file
-whitenoise = rand(44100, 4) - 0.5;
+whitenoise = rand(44100, 4) .- 0.5;
 testbuf = SampleBuf(whitenoise, x.samplerate);
 save("hoge.wav", testbuf);
 
