62c5b7fd9278c0b0a019af21ef8791e9c748836f
JikanBae <jikanbae@mgsoft.org>
2019-01-08 10:23:00 +0900

    used run() instead of copy&paste from the console

--- 20181202_invert_and_cancel/20181202_invert_and_cancel.jl
+++ 20181202_invert_and_cancel/20181202_invert_and_cancel.jl
@@ -4,7 +4,17 @@ import LibSndFile
 using Plots
 pyplot();
 
+run(`afinfo birdland_original.wav`)
+
 sndorg = load("birdland_original.wav")
+
+run(`afconvert -f mp4f birdland_original.wav -o birdland.mp4`);
+
+run(`afinfo birdland.mp4`);
+
+run(`afconvert -f WAVE -d LEI16 birdland.mp4 birdland_aac.wav`);
+
+run(`afinfo birdland_aac.wav`);
 
 sndaac = load("birdland_aac.wav")
 
